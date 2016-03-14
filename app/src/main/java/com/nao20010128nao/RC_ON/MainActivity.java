cc079110c78868d768bc0a75c1c48fe1b4915934
nao20010128nao <nao20010128@gmail.com>
2016-03-14 21:51:59 +0900

    いろいろ

--- app/src/main/java/com/nao20010128nao/RC_ON/MainActivity.java
+++ app/src/main/java/com/nao20010128nao/RC_ON/MainActivity.java
@@ -2,6 +2,12 @@ package com.nao20010128nao.RC_ON;
 
 import android.app.*;
 import android.os.*;
+import android.view.View;
+import android.widget.EditText;
+import java.security.SecureRandom;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.app.NotificationCompat;
+import android.content.Intent;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +16,16 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		findViewById(R.id.start).setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				EditText et=(EditText)findViewById(R.id.mes);
+				NotificationCompat.Builder nb=new NotificationCompat.Builder(MainActivity.this)
+					.setContentTitle("おしゃべりマルチ")
+					.setContentText(et.getText().toString())
+					.setContentIntent(PendingIntent.getActivity(MainActivity.this,0,new Intent(MainActivity.this,MainActivity.class),0))
+					.setSmallIcon(R.drawable.app_icon);
+				((NotificationManager)getSystemService(NOTIFICATION_SERVICE)).notify(Math.abs(new SecureRandom().nextInt()),nb.build());
+			}
+		});
     }
 }
